e54f38a9636cf5723dcad0bfc81778de56d81121
jafar <cefersadiq@gmail.com>
2024-01-07 22:13:45 +0400

    initial setup+9

--- istedadapp/settings.py
+++ istedadapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,15 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", 'django-insecure-4@2ssm*535j4z_m5h(3cny5(@vv7wy_8x*n#d(rjaox3bn069x')
-
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
-ALLOWED_HOSTS = ['164.92.183.253', 'http://164.92.183.253', 'https://164.92.183.253', 'https://istedad21.edu.az', 'https://www.istedad21.edu.az', 'www.istedad21.edu.az', 'istedad21.edu.az']
-
-CSRF_TRUSTED_ORIGINS = ['https://*.istedad21.edu.az']
+DEBUG = str(os.getenv('DEBUG'))
+ALLOWED_HOSTS = ['127.0.0.1']
+if not DEBUG:
+    CSRF_TRUSTED_ORIGINS = ['https://*.istedad21.edu.az']
 
 # Application definition
 
