d35cdc01dec6421e4e6e4742aeb6b5c038ae91d1
jafar <cefersadiq@gmail.com>
2024-01-07 22:18:56 +0400

    initial setup+9

--- istedadapp/settings.py
+++ istedadapp/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = str(os.getenv('DEBUG'))
-ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS'))
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+print((ALLOWED_HOSTS))
 if not DEBUG:
     CSRF_TRUSTED_ORIGINS = ['https://*.istedad21.edu.az']
 
